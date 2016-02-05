8b5fb74122424d65f03852b5320a321a64146aa5
Zhang Qigang <zhangqigang@letv.com>
2016-02-05 11:46:59 +0800

    add carthage support

---
+++ UIImageAdditions/UIImageAdditions.h
@@ -0,0 +1,20 @@
+//
+//  UIImageAdditions.h
+//  UIImageAdditions
+//
+//  Created by Kerberos Zhang on 16/2/5.
+//  Copyright © 2016年 Behaiku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageAdditions.
+FOUNDATION_EXPORT double UIImageAdditionsVersionNumber;
+
+//! Project version string for UIImageAdditions.
+FOUNDATION_EXPORT const unsigned char UIImageAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageAdditions/PublicHeader.h>
+
+
+#import <UIImageAdditions/UIImage+Additions.h>
